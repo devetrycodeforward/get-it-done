9b5483968e38d24b16a281e7a82a176446454ea0
Chris Bay <chris@chrisbay.net>
2017-06-03 18:04:42 -0500

    App db setup and first persistent class

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import os
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+project_dir = os.path.dirname(os.path.abspath(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir, "get-it-done.db"))
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 tasks = []
 
@@ -15,4 +31,5 @@ def index():
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
